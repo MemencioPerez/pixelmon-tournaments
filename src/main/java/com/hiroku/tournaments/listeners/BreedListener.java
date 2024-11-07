baf5c04efca6cd33b354b1621866c3a613eb14c7
Memencio <memencio.perez.2004@gmail.com>
2024-11-07 15:21:30 -0400

    Adds a null check to BreedListener to avoid a NullPointerException caused by AquaDaycare firing a DayCareEvent.PrePokemonAdd event with null Pokemon parents.

--- src/main/java/com/hiroku/tournaments/listeners/BreedListener.java
+++ src/main/java/com/hiroku/tournaments/listeners/BreedListener.java
@@ -3,14 +3,19 @@ package com.hiroku.tournaments.listeners;
 import com.hiroku.tournaments.Tournaments;
 import com.hiroku.tournaments.api.requirements.RentalRequirement;
 import com.pixelmonmod.pixelmon.api.daycare.event.DayCareEvent;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class BreedListener {
 	@SubscribeEvent
 	public void onDayCareAdd(DayCareEvent.PrePokemonAdd event) {
-		if (RentalRequirement.is(event.getParentOne()) || RentalRequirement.is(event.getParentTwo())) {
-			event.setCanceled(true);
-			Tournaments.log(event.getPlayer().getDisplayName().getString() + " is attempting to breed a rental Pokémon!");
+		Pokemon parentOne = event.getParentOne();
+		Pokemon parentTwo = event.getParentTwo();
+		if (parentOne != null && parentTwo != null) {
+			if (RentalRequirement.is(event.getParentOne()) || RentalRequirement.is(event.getParentTwo())) {
+				event.setCanceled(true);
+				Tournaments.log(event.getPlayer().getDisplayName().getString() + " is attempting to breed a rental Pokémon!");
+			}
 		}
 	}
 }
