ffb15212e18d48c11a7d8b057c09d3ab946fee31
Memencio <memencio.perez.2004@gmail.com>
2024-11-08 08:41:57 -0400

    Fix BattleRules#importText method using mixins

--- src/main/java/com/hiroku/tournaments/mixins/BattleRulesMixin.java
+++ src/main/java/com/hiroku/tournaments/mixins/BattleRulesMixin.java
@@ -1,6 +1,7 @@
 package com.hiroku.tournaments.mixins;
 
 import com.pixelmonmod.pixelmon.battles.api.rules.BattleProperty;
+import com.pixelmonmod.pixelmon.battles.api.rules.BattleRuleRegistry;
 import com.pixelmonmod.pixelmon.battles.api.rules.BattleRules;
 import com.pixelmonmod.pixelmon.battles.api.rules.PropertyValue;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,12 +9,36 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Mixin(value = BattleRules.class, remap = false)
 public class BattleRulesMixin {
 
     @Shadow
     private Map<BattleProperty<?>, PropertyValue<?>> properties;
+
+    /**
+     * @author MemencioPerez
+     * @reason Fix instance value assignment by correctly handling {@link Optional} values
+     */
+    @Overwrite
+    public String importText(String text) {
+        String[] importTextSplit = text.split("\n");
+        for (String line : importTextSplit) {
+            String[] split = line.split(": ");
+            BattleProperty<?> property = BattleRuleRegistry.getProperty(split[0]);
+            if (property != null) {
+                PropertyValue<?> instance = property.getInstance();
+                Optional<? extends PropertyValue<?>> parsedPropertyValue = property.parse(split[1]);
+                if (parsedPropertyValue.isPresent()) {
+                    instance = parsedPropertyValue.get();
+                }
+                this.properties.put(property, instance);
+            }
+        }
+
+        return null;
+    }
 
     /**
      * @author MemencioPerez
